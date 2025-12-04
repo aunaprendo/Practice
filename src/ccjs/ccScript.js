e74f9227c79ade1abec6890da2aa8d7a52fb8145
Georgia Jean <236709901+aunaprendo@users.noreply.github.com>
2025-12-03 21:29:36 -0400

    j
    H

--- src/ccjs/ccScript.js
+++ src/ccjs/ccScript.js
@@ -122,7 +122,7 @@ function closeMenu() {
 }
 
 
-document.querySelectorAll('.dropbtn, .dropbtn-second').forEach(btn => {
+document.querySelectorAll('.dropbtn').forEach(btn => {
   btn.addEventListener('click', () => {
     const expanded = btn.getAttribute('aria-expanded') === 'true';
     btn.setAttribute('aria-expanded', !expanded);
