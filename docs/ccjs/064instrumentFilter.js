8b1c7b8f8f02e24126dac322fb7a654ad340c8a4
Georgia Jean <236709901+aunaprendo@users.noreply.github.com>
2026-02-20 22:45:16 -0400

    t

--- docs/ccjs/064instrumentFilter.js
+++ docs/ccjs/064instrumentFilter.js
@@ -1,26 +1,36 @@
 const instrumentsArr = [
-  { category: "woodwinds", instrument: "Flute", price: 500 },
-  { category: "woodwinds", instrument: "Clarinet", price: 200 },
-  { category: "woodwinds", instrument: "Oboe", price: 4000 },
-  { category: "brass", instrument: "Trumpet", price: 200 },
-  { category: "brass", instrument: "Trombone", price: 300 },
-  { category: "brass", instrument: "French Horn", price: 4300 },
-  { category: "percussion", instrument: "Drum Set", price: 500 },
-  { category: "percussion", instrument: "Xylophone", price: 3000 },
-  { category: "percussion", instrument: "Cymbals", price: 200 },
-  { category: "percussion", instrument: "Marimba", price: 3000 }
+  { category: "woodwinds", instrument: "Acorn Flute", price: 500 },
+  { category: "woodwinds", instrument: "Hazelnut Clarinet", price: 200 },
+  { category: "woodwinds", instrument: "Pinecone Oboe", price: 4000 },
+  { category: "brass", instrument: "Chestnut Trumpet", price: 200 },
+  { category: "brass", instrument: "Walnut Trombone", price: 300 },
+  { category: "brass", instrument: "Golden Acorn French Horn", price: 4300 },
+  { category: "percussion", instrument: "Log Drum Set", price: 500 },
+  { category: "percussion", instrument: "Twig Xylophone", price: 3000 },
+  { category: "percussion", instrument: "Shell Cymbals", price: 200 },
+  { category: "percussion", instrument: "Forest Marimba", price: 3000 }
 ]
 
 const selectContainer = document.querySelector("select");
 const productsContainer = document.querySelector(".products-container");
-
 function instrumentCards(instrumentCategory) {
-  if (instrumentCategory === "all") {
-  	return instrumentsArr;
-  } else {
-  	return instrumentsArr.filter((item) => item.category === instrumentCategory);
-  }
+  const instruments =
+    instrumentCategory === "all"
+      ? instrumentsArr
+      : instrumentsArr.filter(
+          ({ category }) => category === instrumentCategory
+        );
+
+  return instruments
+    .map(({ instrument, price }) => {
+      return `
+          <div class="Mcard">
+            <h2>${instrument}</h2>
+            <p>$${price}</p>
+          </div>
+        `;
+    }).join("")
 }
-
-selectContainer.addEventListener("change", () => instrumentCards(selectContainer.value))
-
+selectContainer.addEventListener("change", () => {
+  productsContainer.innerHTML = instrumentCards(selectContainer.value);
+});
--- docs/ccjs/notes.js
+++ docs/ccjs/notes.js
@@ -1,8 +1,38 @@
+const instrumentsArr = [
+  { category: "woodwinds", instrument: "Flute", price: 500 },
+  { category: "woodwinds", instrument: "Clarinet", price: 200 },
+  { category: "woodwinds", instrument: "Oboe", price: 4000 },
+  { category: "brass", instrument: "Trumpet", price: 200 },
+  { category: "brass", instrument: "Trombone", price: 300 },
+  { category: "brass", instrument: "French Horn", price: 4300 },
+  { category: "percussion", instrument: "Drum Set", price: 500 },
+  { category: "percussion", instrument: "Xylophone", price: 3000 },
+  { category: "percussion", instrument: "Cymbals", price: 200 },
+  { category: "percussion", instrument: "Marimba", price: 3000 }
+]
 
+const selectContainer = document.querySelector(".select-container");
+const productsContainer = document.querySelector(".products-container");
 
+function instrumentCards(instrumentCategory) {
+  let filteredItems;
 
-Within your new function, you need to filter the instruments depending on the selected category.
+  if (instrumentCategory === "all") {
+    filteredItems = instrumentsArr;
+  } else {
+    filteredItems = instrumentsArr.filter(
+      (item) => item.category === instrumentCategory
+    );
+  }
+  return filteredItems.map(
+    (item) =>
+      `<div class="card"><h2>${item.instrument}</h2><p>$${item.price}</p></div>`
+  ).join("");
+	
+}
 
-Filter out items from instrumentsArr and make your function return an array containing the instrument objects with the same category of instrumentCategory. If instrumentCategory is equal to all, return the whole instrumentsArr array.
+selectContainer.addEventListener("change", (event) => {
+  const filteredCards = instrumentCards(event.target.value); 
+	productsContainer.innerHTML = filteredCards;
+});
 
-Then, remove the console.log from the callback of your event listener and log the result of calling instrumentCards with the selected option from the dropdown menu as argument so you can test your function selecting different category options.
