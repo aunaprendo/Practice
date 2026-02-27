c216483ff8f7a1be05f94675f1a822511d28bdf8
Georgia Jean <236709901+aunaprendo@users.noreply.github.com>
2026-02-26 21:58:59 -0400

    k

--- docs/ccjs/071themeSwitch.js
+++ docs/ccjs/071themeSwitch.js
@@ -1,5 +1,36 @@
 const themes =[
-	{name: Squirrels, message: commense nuts}
-	{name: Dinosaurs, message: RAWR}	
+	{name: "squirrels", message: "Nuts Hidden 🌰"},
+	{name: "dinosaurs", message: "RAWR 🦖"},
+	{name: "trees", message: "Drey Built 🌳"}	
 ];
 
+const btn = document.getElementById("theme-switcher-button");
+const menu = document.getElementById("theme-dropdown");
+const body = document.querySelector("body");
+const message = document.getElementById("message");
+const styles = document.querySelectorAll("ul li");
+
+btn.addEventListener("click", () => {
+	if (menu.hasAttribute("hidden")) {
+		menu.removeAttribute("hidden");
+		btn.setAttribute("aria-expanded", "true")
+	} else {
+		menu.setAttribute("hidden", "");
+		btn.setAttribute("aria-expanded", "false")
+	};
+});
+
+styles.forEach(style => {
+	style.addEventListener("click", () => {
+		
+	for (let i = 0; i < themes.length; i++) {
+		if (themes[i].name === style.innerText) {
+			message.innerText = themes[i].message;
+			body.setAttribute("class", `theme-${style.innerText}`);
+			setTimeout(() => {message.innerText="";}, 6000);
+		}
+	}
+	
+});
+});
+		
--- docs/ccjs/notes.js
+++ docs/ccjs/notes.js
@@ -4,11 +4,4 @@
 
 
 
-
-
-
-
-When a user clicks on the #theme-switcher-button, the #theme-dropdown element should display the available themes.
-When the #theme-dropdown element is displayed, the aria-expanded attribute should be set to "true" for the button element.
-When the users clicks on the #theme-switcher-button while the #theme-dropdown element is displayed, the menu should be closed and the hidden attribute should be added and the aria-expanded attribute should be set to "false".
-When a user clicks on the #theme-switcher-button and selects a theme, the corresponding theme-<name> class should be added to the body element and the related theme message you set in the themes array should display in the aria-live="polite" element.
+26. When a user clicks on the #theme-switcher-button and selects a theme, the corresponding theme-<name> class should be added to the body element.
